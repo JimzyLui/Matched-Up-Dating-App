387ad91b9581aa1eab6e880725f259f599d84b7c
Jimzy Lui <jimzylui_desktop@stickyaznrabbit.com>
2013-12-07 14:10:47 -0500

    Initial Commit

---
+++ MatchedUp/MUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MUAppDelegate.h
+//  MatchedUp
+//
+//  Created by Jimzy Lui on 12/7/2013.
+//  Copyright (c) 2013 Jimzy Lui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
